18998bc43e2ae263bb4d0fad270ffab471566757
Zormolo <max.renner96@web.de>
2026-01-04 19:03:23 +0100

    Banner Timer added, Tabs for diff Info for Banner Costume, Refactoring of Names and Function-Calls, responsive design upgraded

--- public/js/generateSite.js
+++ public/js/generateSite.js
@@ -1,50 +1,139 @@
-function getBannerCards( jsonData ) {
-  const cardArray = [];
+let timeLeftUpdateInterval = null;
+const timeLeftArray = [];
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+async function createBannerCards( bannerData ) {
+  /** @type { HTMLDivElement } */
+  const container = document.getElementById( "main-container" );
 
   /** @type { HTMLTemplateElement } */
   const template = document.getElementById( 'charBannerCard' );
 
-  for( const bannerChar of jsonData[ "banner" ] ) {
+  for( const bannerChar of bannerData ) {
     const bannerCard = template.content.cloneNode( true );
 
+    //Tabs
+    const basicTab = bannerCard.querySelector( '#basicTab' );
+    basicTab.dataset.bsTarget = `#basic_${ bannerChar.imgName }`;
+    basicTab.removeAttribute( 'id' );
+    const basicTabContent = bannerCard.querySelector( '#basic' );
+    basicTabContent.id = `basic_${ bannerChar.imgName }`;
+
+    const pacTab = bannerCard.querySelector( '#pacTab' );
+    pacTab.dataset.bsTarget = `#pac_${ bannerChar.imgName }`;
+    pacTab.removeAttribute( 'id' );
+    const pacTabContent = bannerCard.querySelector( '#pac' );
+    pacTabContent.id = `pac_${ bannerChar.imgName }`;
+
+    const modeTab = bannerCard.querySelector( '#modeTab' );
+    modeTab.dataset.bsTarget = `#mode_${ bannerChar.imgName }`;
+    modeTab.removeAttribute( 'id' );
+    const modeTabContent = bannerCard.querySelector( '#mode' );
+    modeTabContent.id = `mode_${ bannerChar.imgName }`;
+
+    //Basic Info
     /** @type { HTMLImageElement } */
     const costumeImg = bannerCard.querySelector(".costumeImg");
     costumeImg.src = `./public/images/costumes/${ bannerChar.imgName }.png`;
     costumeImg.alt = bannerChar.imgName;
     costumeImg.title = bannerChar.costumeName;
+    costumeImg.classList.remove( "costumeImg" );
 
     const cardTitle = bannerCard.querySelector(".bannerName");
     const title = document.createElement( 'h1' );
     title.textContent = `${ bannerChar.costumeName } ${ bannerChar.charName }`;
     cardTitle.appendChild( title );
+    cardTitle.classList.remove( 'bannerName' );
 
-    bannerCard.querySelector(".role").innerHTML += bannerChar.role;
+    const roleLine = bannerCard.querySelector(".role")
+    const roleText = document.createTextNode( bannerChar.role );
+    roleLine.appendChild( roleText );
+    roleLine.classList.remove( 'role' );
 
     const propertyImg = bannerCard.querySelector(".property");
     propertyImg.src = `./public/images/${ bannerChar.element }.png`;
     propertyImg.alt = bannerChar.element;
     propertyImg.title = bannerChar.element;
+    propertyImg.classList.remove( 'property' );
 
-    bannerCard.querySelector(".dmgType").innerHTML += bannerChar.dmgType;
+    const dmgTypeLine = bannerCard.querySelector(".dmgType")
+    const dmgTypeText = document.createTextNode( bannerChar.dmgType );
+    dmgTypeLine.appendChild( dmgTypeText );
+    dmgTypeLine.classList.remove( 'dmgType' );
 
-    bannerCard.querySelector(".date").innerHTML += getBannerDateString( bannerChar.startDate, bannerChar.endDate );
+    const startDate = new Date( Date.parse( bannerChar.startDate ) );
+    const endDate = new Date( Date.parse( bannerChar.endDate ) );
+    const periodeLine = bannerCard.querySelector( ".bannerPeriode" );
+    const periodeText = document.createTextNode( getBannerPeriodeLocalTimeString( startDate, endDate ) );
+    periodeLine.appendChild( periodeText );
+    periodeLine.classList.remove( 'bannerPeriode' );
 
-    bannerCard.querySelector( ".breakpoints" ).innerHTML = getBreakpoints( bannerChar.breakpoints );
+    const timeLeftLine = bannerCard.querySelector( ".bannerTimeLeft" );
+    const timeLeftContainer = getTimeLeftSpan( endDate );
+    const timeLeftStartText = document.createTextNode( 'Banner ends in ' );
+    const timeLeftEndText = document.createTextNode( ' !' );
+    timeLeftLine.appendChild( timeLeftStartText );
+    timeLeftLine.appendChild( timeLeftContainer );
+    timeLeftLine.appendChild( timeLeftEndText );
+    timeLeftLine.classList.remove( 'bannerTimeLeft' );
+    timeLeftArray.push( [ endDate, timeLeftContainer ] );
 
-    bannerCard.querySelector( ".pullReason" ).innerHTML = bannerChar.pullReason;
+    const breakpoints = bannerCard.querySelector( ".breakpoints" )
+    breakpoints.innerHTML = getBreakpoints( bannerChar.breakpoints );
+    breakpoints.classList.remove( 'breakpoints' );
 
-    cardArray.push( bannerCard );
+    const pullReason = bannerCard.querySelector( ".pullReason" )
+    pullReason.innerHTML = bannerChar.pullReason;
+    pullReason.classList.remove( 'pullReason' );
+
+    //Pros and Cons
+    const pros = bannerCard.querySelector( ".pros" );
+    addListElements( pros, bannerChar.pros );
+    pros.classList.remove( 'pros' );
+
+    const cons = bannerCard.querySelector( ".cons" );
+    addListElements( cons, bannerChar.cons );
+    cons.classList.remove( 'cons' );
+
+    container.appendChild( bannerCard );
   }
 
-  return cardArray;
+  template.remove();
 }
 
-function getBannerDateString( start, end ) {
-  const startDate = new Date( Date.parse( start ) );
-  const endDate = new Date( Date.parse( end ) );
-  const periode = `<li>${ startDate.toLocaleDateString() } &#8212; ${ endDate.toLocaleDateString() }</li>`;
 
-  let dateDiff = endDate.getTime() - new Date().getTime();
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function getBannerPeriodeLocalTimeString( start, end ) {
+  return `
+    ${ start.toLocaleDateString() } ${ String.fromCharCode( 8212 ) } ${ end.toLocaleDateString() }
+  `;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function getTimeLeftSpan( end ) {
+  const timeLeft = calcTimeLeftOnBanner( end );
+
+  let textColor = "text-warning";
+  if ( timeLeft[0] < 4 ) {
+    textColor = "text-danger"
+  }
+
+  const timeLeftContainer = document.createElement( 'span' );
+  timeLeftContainer.classList.add( textColor );
+  timeLeftContainer.textContent = createTimeLeftString( timeLeft );
+
+  return timeLeftContainer;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function calcTimeLeftOnBanner( end ) {
+  let dateDiff = end.getTime() - new Date().getTime();
 
   const daysLeft = Math.trunc( dateDiff / ( 1000 * 60 * 60 * 24 ) );
   dateDiff %= ( 1000 * 60 * 60 * 24 );
@@ -54,33 +143,75 @@ function getBannerDateString( start, end ) {
 
   const minutesLeft = Math.trunc( dateDiff / ( 1000 * 60 ) );
 
-  let textColor = "text-warning";
-  if ( daysLeft < 4 ) {
-    textColor = "text-danger"
-  }
-
-  const timeLeft = `<li>Banner ends in <span class="${ textColor }">${ daysLeft } D : ${ hoursLeft } h : ${ minutesLeft } min</span> !</li>`;
-  return periode + timeLeft;
+  return [ daysLeft, hoursLeft, minutesLeft ];
 }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function createTimeLeftString( [ days, hours, minutes ] ) {
+  const daysLeft = String( days ).padStart( 2, '0' );
+  const hoursLeft = String( hours ).padStart( 2, '0' );
+  const minutesLeft = String( minutes ).padStart( 2, '0' );
+
+  return `${ daysLeft } D : ${ hoursLeft } h : ${ minutesLeft } min`;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function getBreakpoints( breakpoints ) {
   let breakpointString = "";
 
   for( const [dupe, comment] of breakpoints ) {
-    breakpointString += `<li class="list-group-item"><img class="pe-2" src="./public/images/${ dupe }.png" width="40px" height="32px"> ${ comment }</li>`;
+    breakpointString += `<li class="list-group-item"><img class="pe-2" src="./public/images/${ dupe }.png" 
+      loading="lazy" title="Needed Copies: ${ dupe + 1 }" alt="${ dupe + 1 }_dupes.png"
+      width="40px" height="32px"> ${ comment }</li>`;
   }
+
   return breakpointString;
 }
 
+
+function addListElements( list, dataArray ) {
+  if ( dataArray === undefined || dataArray.length === 0 ) {
+    const listElement = document.createElement( 'li' );
+    listElement.classList.add( 'list-group-item' );
+    listElement.textContent = "Nothing noteworthy!";
+    list.appendChild( listElement );
+    return;
+  }
+
+  for ( const point of dataArray ) {
+    const listElement = document.createElement( 'li' );
+    listElement.classList.add( 'list-group-item' );
+    listElement.textContent = point;
+    list.appendChild( listElement );
+  }
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function updateBannerTimeLeft() {
+  for( const [ endDate, span ] of timeLeftArray ) {
+    const timeLeft = calcTimeLeftOnBanner( endDate );
+    span.textContent = createTimeLeftString( timeLeft );
+  }
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 async function init() {
   const res = await fetch( "./public/json/data.json" );
-  const data = await res.json();
+  const jsonData = await res.json();
 
-  /** @type { HTMLDivElement } */
-  const container = document.getElementById( "main-container" );
-  for( const bannerCard of getBannerCards( data ) ) {
-    container.appendChild( bannerCard );
-  }
-  console.log(data)
+  await createBannerCards( jsonData[ 'banner' ] );
+
+  const now = new Date();
+  const msUntilNextMin = ( 60 - now.getSeconds() ) * 1000 - now.getMilliseconds();
+  setTimeout( () => {
+    updateBannerTimeLeft();
+    timeLeftUpdateInterval = setInterval( updateBannerTimeLeft, 60000 );
+  }, msUntilNextMin );
 }
 init();
